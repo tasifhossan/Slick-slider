3b83de1fc799f261645c74ecb3e9ae15b67a4773
Mohammad Tasif Hossan <153765460+tasifhossan@users.noreply.github.com>
2024-04-13 14:46:37 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+// jq
+$(document).ready(function(){
+
+$('.wrapper').slick({dots: true});
+
+$('.wrapper1').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+});
